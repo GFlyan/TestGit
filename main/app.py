aa2b1dd7b7f47a832f1afd2dde83a2f4414c48c7
GFlyan <guilhermeflyan.22@gmail.com>
2025-05-02 13:54:39 -0300

    Update app.py

--- main/app.py
+++ main/app.py
@@ -2,32 +2,22 @@ import ipeadatapy as ipea
 import streamlit as st
 import pandas as pd
 
-# "* TODOS OS TEMAS"
-# themes = ipea.themes()
-# themes
+df = ipea.sources()
+df
+bota = "bacen"
+def organization(phrase: str):
+    """
+    Retorna um dataframe contendo as series do IPEA de acordo com a string parametrizada referente a um órgão procurado.
 
-def searchTheme(phrase: str):
-    phrase = phrase.lower()
-    getThemeID = ipea.themes()
-    getThemeID['NAME'] = getThemeID['NAME'].str.lower()
-    getThemeID = getThemeID[getThemeID['NAME'].str.contains(phrase)]
-
-    try:
-        final = pd.DataFrame()
-        for id in getThemeID['ID']:
-            theme = ipea.metadata(theme_id=id)
-            final = pd.concat([final, theme], ignore_index=True)
-
-        allSeries = final.sort_values(by='CODE')
-    except:
-        return str("Não Encontrado")
-    else:
-        return allSeries
-
-"* PESQUISA POR TEMAS"
-testDataFrame = searchTheme("demografia")
-testDataFrame
+    Caso a busca não seja bem sucedida sera retornado uma string "Não Encotrado".
+    """
+    series = ipea.metadata()
+    series = series[series["MEASURE"].str.contains("\\$")]
+    series = pd.concat([series[series["SOURCE ACRONYM"].str.lower().str.contains(phrase.lower())],
+                        series[series["SOURCE"].str.lower().str.contains(phrase.lower())]])
+    series = series.sort_values(by='CODE').drop_duplicates()
+    return "Não Encontrado" if series.empty else series
 
 
-
-
+teste = organization(bota)
+teste
